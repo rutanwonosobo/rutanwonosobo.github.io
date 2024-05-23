7482bd8e0a5c8ecd0e0b1bb283d10f00e5ff702b
Eliyanto Sarage <tikom.rutanwonosobo@gmail.com>
2024-05-23 11:49:15 +0700

    Create all.js

---
+++ assets/js/all.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    // Inisialisasi popover
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl);
+    });
+
+    // Set lazyload
+    $('img').each(function () {
+        $(this).attr('loading', 'lazy');
+    });
+});
+
+document.querySelector('.info-wbp marquee').addEventListener('mouseover', function () {
+    this.stop();
+});
+
+document.querySelector('.info-wbp marquee').addEventListener('mouseout', function () {
+    this.start();
+});
