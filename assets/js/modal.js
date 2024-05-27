71530eca653d2f5ce4e09a8914daa2af3cc9479e
Eliyanto Sarage <tikom.rutanwonosobo@gmail.com>
2024-05-28 06:41:43 +0700

    Create modal.js

---
+++ assets/js/modal.js
@@ -0,0 +1,206 @@
+// Pengaturan Modal Sejarah Rutan
+var SejarahRutanModal = `
+<div class="modal fade" id="SejarahRutanModal" tabindex="-1" aria-labelledby="SejarahRutanModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="fs-6 modal-title text-uppercase fw-bold" id="SejarahRutanModalLabel"><i class="bi bi-building-fill me-2"></i>Sejarah Rutan Wonosobo</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+                <p><strong>Unit Pelaksana Teknis (UPT) Pemasyarakatan</strong> di daerah yang bertanggung jawab langsung kepada <strong>Kepala Kantor Wilayah Kementerian Hukum dan HAM Jawa Tengah</strong>. Rutan Wonosobo berdomisili di <strong>Jalan Pramuka Nomor 1 Wonosobo</strong>, dengan luas tanah 4.680 m² dan luas bangunan 1.268 m², memiliki kapasitas 150 orang, dan merupakan bangunan yang dibangun sejak tahun 1860 yang usianya sudah sangat tua dan terakhir dilakukan rehabilitasi pada tahun 1998.</p>
+                <hr>
+                <p>Secara geografis, <strong>Kabupaten Wonosobo</strong> berjarak sekitar 130 km dari <strong>ibu kota provinsi (Semarang)</strong> dan berjarak sekitar 520 km dari <strong>ibu kota Jakarta</strong>, dengan ketinggian berkisar antara 270 m hingga 2.250 m di atas permukaan laut, dan tergolong sebagai daerah yang memiliki kelembaban udara sangat tinggi.</p>
+                <hr>
+                <p><strong>Rutan Wonosobo</strong> terletak tidak jauh dari <strong>pusat kota Wonosobo (Alun-alun Wonosobo)</strong> yang berjarak sekitar 50 m, lokasinya sangat strategis dan mudah dijangkau karena terletak di lingkungan pusat kota dan dekat dengan jalur angkutan umum.</p>
+                <hr>
+                <p>Dalam kerangka sistem peradilan pidana, <strong>Rutan Wonosobo</strong> letaknya sangat dekat dengan <strong>Kejaksaan Negeri Wonosobo</strong>, kurang lebih 20 m, sedangkan jaraknya dengan <strong>Polres Wonosobo</strong> kurang lebih 1 km, dan jaraknya dengan <strong>Pengadilan Negeri Wonosobo</strong> kurang lebih 2 km.</p>
+                <hr>
+                <p>Komponen bangunan <strong>Rutan Wonosobo</strong> yang merupakan bangunan zaman Belanda belum memenuhi standar yang ditentukan sebagai bangunan <strong>Unit Pelaksana Teknis (UPT)</strong> yang berpedoman pada sistem pemasyarakatan. Di antaranya, blok hunian yang ada terdiri dari 1 blok pria dengan 14 kamar yang dipergunakan untuk blok tahanan sebanyak 7 kamar dan blok narapidana sebanyak 7 kamar, sedangkan 1 blok dipergunakan untuk hunian tahanan dan narapidana wanita yang terdiri dari 1 kamar.</p>
+                <hr>
+                <p>Letak <strong>Rumah Dinas Kepala Rutan</strong> cukup jauh dari lokasi Rutan, yakni berada di <strong>Jalan Kyai Sabuk Alu Wonosobo</strong> yang berjarak sekitar 1 km dari Rutan Wonosobo.</p>
+            </div>
+            <div class="modal-footer justify-content-center">
+                <a href="https://rutanwonosobo.kemenkumham.go.id/profil/tentang-satuan-kerja/selayang-pandang-satuan-kerja" class="btn btn-primary btn-sm w-100" role="button" target="_blank">SELAYANG PANDANG SATUAN KERJA</a>
+            </div>
+        </div>
+    </div>
+</div>
+`;
+
+document.write(SejarahRutanModal);
+
+// Pengaturan Modal Tentang Aplikasi
+var AboutModal = `
+<div class="modal fade" id="AboutModal" tabindex="-1" aria-labelledby="AboutModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+        <div class="modal-header d-flex justify-content-center align-items-center">
+            <h5 class="fs-6 modal-title text-uppercase fw-bold" id="AboutModalLabel">
+                <i class="bi bi-file-earmark-person-fill me-2"></i>Tentang Sayap Tawon
+                <h6><span
+                        class="badge rounded-pill text-bg-primary font-monospace fst-italic ms-2 px-3">pre-release</span>
+                </h6>
+            </h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body text-center">
+            <div class="row">
+                <h3 class="h2 fw-bold text-uppercase mt-2 mb-4 pb-4 text-secondary border-bottom border-2">
+                    Development Team
+                </h3>
+                <div
+                    class="col-md-6 ms-auto d-flex flex-column justify-content-center align-items-center author">
+                    <h5 class="h4 text-uppercase fw-bold mb-3">Karutan Wonosobo</h5>
+                    <img src="https://cdn.jsdelivr.net/gh/rutanwonosobo/rutanwonosobo.github.io@main/assets/img/narya.webp"
+                        alt="Narya, Amd. IP., S.H." class="img-fluid rounded shadow-sm">
+                    <p class="fs-6 fw-bold my-2">Narya, Amd. IP., S.H.</p>
+                    <p class="fst-italic mb-4">(App Initiator)</p>
+                </div>
+                <div
+                    class="col-md-6 ms-auto d-flex flex-column justify-content-center align-items-center author">
+                    <h5 class="h4 text-uppercase fw-bold mb-3">Anggota Regu Pengamanan</h5>
+                    <img src="https://cdn.jsdelivr.net/gh/rutanwonosobo/rutanwonosobo.github.io@main/assets/img/eliyantosarage.webp"
+                        alt="Eliyanto Sarage" class="img-fluid rounded shadow">
+                    <p class="fs-6 fw-bold my-2">Eliyanto Sarage</p>
+                    <p class="fst-italic mb-4">(App Developer)</p>
+                </div>
+            </div>
+            <hr>
+            <p><strong>Sistem Layanan Terpadu Rutan Wonosobo</strong> adalah sebuah sistem yang mengintegrasikan
+                berbagai layanan publik yang ada di Rutan Wonosobo dalam satu platform. Sistem ini bertujuan
+                untuk memudahkan masyarakat dalam mengakses layanan publik di Rutan Wonosobo, meningkatkan
+                kualitas pelayanan publik, dan mewujudkan transparansi dan akuntabilitas penyelenggaraan layanan
+                publik di Rutan Wonosobo.
+            </p>
+            <hr>
+            <p><strong>Sistem Layanan Terpadu Rutan Wonosobo</strong> dikembangkan menggunakan framework <button
+                    type="button" class="btn btn-sm fw-bold btn-primary rounded-pill px-3"
+                    data-bs-toggle="popover" data-bs-title="Bootstrap"
+                    data-bs-content="Bootstrap adalah framework front-end gratis dan open-source yang digunakan untuk mengembangkan proyek responsif seluler di web. Bootstrap adalah kerangka kerja HTML, CSS, dan JS yang paling populer."><i
+                        class="bi bi-bootstrap me-2"></i>Bootstrap</button> untuk memastikan
+                responsivitas antarmuka pengguna yang optimal. Kode aplikasi disimpan dan dikelola melalui
+                platform <button type="button" class="btn btn-sm fw-bold btn-dark rounded-pill px-3"
+                    data-bs-toggle="popover" data-bs-title="GitHub"
+                    data-bs-content="GitHub adalah sebuah platform online berbasis cloud yang digunakan oleh para developer untuk menyimpan, melacak, dan mengelola kode dan hasil proyek IT. GitHub juga merupakan tempat kolaborasi antar developer dalam suatu proyek perangkat lunak."><i
+                        class="bi bi-github me-2"></i>GitHub</button>, yang memfasilitasi kolaborasi tim
+                dan pelacakan perubahan dengan efisiensi. Selain itu, proses deployment aplikasi dilakukan
+                melalui platform <button type="button" class="btn btn-sm fw-bold btn-dark rounded-pill px-3"
+                    data-bs-toggle="popover" data-bs-title="Vercel"
+                    data-bs-content="Vercel adalah platform cloud Amerika yang memungkinkan pengembang untuk membangun, menerapkan, dan menskalakan situs web statis dan aplikasi tanpa server. Vercel sebelumnya dikenal sebagai ZEIT dan fokus utamanya adalah menyediakan hosting untuk aplikasi Node.js."><i
+                        class="bi bi-triangle-fill me-2"></i>Vercel</button>, memanfaatkan integrasi yang ada
+                untuk memastikan ketersediaan dan kinerja yang optimal. Dengan demikian, pendekatan teknologi
+                modern yang digunakan dalam pembuatan aplikasi ini memberikan landasan yang kokoh untuk
+                penyediaan layanan terpadu di Rutan Wonosobo.
+            </p>
+        </div>
+        <div class="modal-footer justify-content-center">
+            <small>Powered by <a href="https://www.instagram.com/eliyantosarage_/" target="_blank"
+                    class="text-muted fst-italic">@eliyantosarage</a></small>
+        </div>
+    </div>
+</div>
+</div>
+`;
+
+document.write(AboutModal);
+
+// Pengaturan Modal Telephone
+var phoneModal = `
+<div class="modal fade" id="phoneModal" tabindex="-1" aria-labelledby="phoneModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-sm modal-dialog-centered">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="fs-6 modal-title text-uppercase fw-bold" id="phoneModalLabel"><i
+                    class="bi bi-telephone-fill me-2"></i>Telephone</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body text-center fw-bold">
+            (0286) 321030
+        </div>
+    </div>
+</div>
+</div>
+`;
+
+document.write(phoneModal);
+
+// Pengaturan Modal Faximile
+var faxModal = `
+<div class="modal fade" id="faxModal" tabindex="-1" aria-labelledby="faxModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="fs-6 modal-title text-uppercase fw-bold" id="faxModalLabel"><i
+                        class="bi bi-printer-fill me-2"></i>Faximile
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center fw-bold">
+                (0286) 321384
+            </div>
+        </div>
+    </div>
+</div>
+`;
+
+document.write(faxModal);
+
+// Pengaturan Modal WhatsApp
+var whatsappModal = `
+<div class="modal fade" id="whatsappModal" tabindex="-1" aria-labelledby="whatsappModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="fs-6 modal-title text-uppercase fw-bold" id="whatsappModalLabel"><i
+                        class="bi bi-whatsapp me-2"></i>Aduan & Informasi</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center fw-bold">
+                (+62) 895-1072-5657
+            </div>
+            <div class="modal-footer justify-content-center">
+                <a href="https://api.whatsapp.com/send?phone=6289510725657" class="btn btn-primary btn-sm w-100"
+                    role="button" target="_blank">open in whatsapp</a>
+            </div>
+        </div>
+    </div>
+</div>
+`;
+
+document.write(whatsappModal);
+
+// Pengaturan Modal Pengumuman
+var modalPengumuman = `
+<div class="modal fade" id="modalPengumuman" tabindex="-1" aria-labelledby="modalPengumumanLabel"
+aria-hidden="true">
+<div class="modal-dialog">
+    <div class="modal-content text-center">
+        <div class="modal-header">
+            <h5 class="fs-6 modal-title text-uppercase fw-bold" id="modalPengumumanLabel">Pengumuman</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <!-- Isi Pengumuman -->
+        </div>
+    </div>
+</div>
+</div>
+`;
+
+document.write(modalPengumuman);
+
+// Pengaturan Tombol Pengumuman
+var badgePengumuman = `
+<div class="pengumuman sticky-bottom d-block d-lg-none text-center">
+<button type="button" class="btn btn-primary position-relative rounded-circle">
+    <i class="bi bi-bell-fill"></i>
+    <span class="position-absolute top-0 end-0 translate-middle-y badge shadow-sm rounded-circle bg-light"
+        id="badgePengumuman">
+        <i class="bi bi-1-circle-fill text-danger shadow-sm fs-6"></i>
+    </span>
+</button>
+</div>
+`;
+
+document.write(badgePengumuman);
