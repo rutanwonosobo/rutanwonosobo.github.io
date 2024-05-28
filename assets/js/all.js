22829876b8af07b9e3329bc2ec51bcce69434844
Rutan Wonosobo <tikom.rutanwonosobo@gmail.com>
2024-05-28 13:07:35 +0700

    Update all.js

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,25 +1,36 @@
-$(document).ready(function () {
+$(document).ready(function() {
     // Inisialisasi popover
     var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
         return new bootstrap.Popover(popoverTriggerEl);
     });
 
     // Set lazyload
-    $('img').each(function () {
+    $('img').each(function() {
         $(this).attr('loading', 'lazy');
     });
 });
 
-document.querySelector('.info-wbp marquee').addEventListener('mouseover', function () {
+document.querySelector('.info-penghuni marquee').addEventListener('mouseover', function() {
     this.stop();
 });
 
-document.querySelector('.info-wbp marquee').addEventListener('mouseout', function () {
+document.querySelector('.info-penghuni marquee').addEventListener('mouseout', function() {
     this.start();
 });
-$(document).ready(function () {
-    $('.modal').on('show.bs.modal', function () {
+$(document).ready(function() {
+    $('.modal').on('show.bs.modal', function() {
         $('.modal').not($(this)).modal('hide');
+    });
+});
+// Mendapatkan tinggi maksimum dari kedua tbody
+window.addEventListener('load', function() {
+    var tbody1Height = document.querySelector('.col-md-6:nth-child(1) tbody').offsetHeight;
+    var tbody2Height = document.querySelector('.col-md-6:nth-child(2) tbody').offsetHeight;
+    var maxTbodyHeight = Math.max(tbody1Height, tbody2Height);
+
+    // Set tinggi semua tbody menjadi tinggi maksimum
+    document.querySelectorAll('.table-wrapper tbody').forEach(function(tbody) {
+        tbody.style.height = maxTbodyHeight + 'px';
     });
 });
