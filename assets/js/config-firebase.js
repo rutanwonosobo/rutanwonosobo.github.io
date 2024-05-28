94b7c3146c6321997fa1876cf21625e2d93130ea
Rutan Wonosobo <tikom.rutanwonosobo@gmail.com>
2024-05-29 00:13:31 +0700

    Update config-firebase.js

--- assets/js/config-firebase.js
+++ assets/js/config-firebase.js
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// Konfigurasi Firebase
+var firebaseConfig = {
   apiKey: "AIzaSyAQqCb4obTgFMozVOg3JqJJ-ZCmdPhITLs",
   authDomain: "rutan-wonosobo-0.firebaseapp.com",
   projectId: "rutan-wonosobo-0",
@@ -13,7 +8,32 @@ const firebaseConfig = {
   appId: "1:587999781707:web:7fb68389a77364bb9a7ece",
   measurementId: "G-2ZW152V3VP"
 };
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
+// Referensi ke Firebase Database
+var database = firebase.database();
+var visitorsRef = database.ref('visitors');
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Fungsi untuk menambahkan pengunjung
+function addVisitor() {
+  var today = new Date().toISOString().slice(0, 10);
+  var path = 'statistics/' + today;
+  visitorsRef.child(path).transaction(function (currentValue) {
+    return (currentValue || 0) + 1;
+  });
+}
+
+// Fungsi untuk menampilkan statistik pengunjung
+function displayVisitorStats() {
+  var totalRef = visitorsRef.child('total');
+  totalRef.on('value', function (snapshot) {
+    var totalVisitors = snapshot.val() || 0;
+    document.getElementById('totalVisitors').textContent = totalVisitors;
+  });
+}
+
+// Panggil fungsi ini saat halaman dimuat
+window.onload = function () {
+  addVisitor();
+  displayVisitorStats();
+};
