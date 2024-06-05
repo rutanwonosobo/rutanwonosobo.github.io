9cde1ce95146d0c2d1d52e89e69c6df9950371bc
Humas Rutan Wonosobo <tikom.rutanwonosobo@gmail.com>
2024-06-05 18:42:15 +0700

    Update informasi-penghuni.js

--- assets/js/informasi-penghuni.js
+++ assets/js/informasi-penghuni.js
@@ -1,14 +1,25 @@
-// Data yang ingin diatur
-var totalWBP = 141;
-var jumlahTahanan = 57;
-var lakiLakiTahanan = 55;
-var perempuanTahanan = 2;
-var jumlahNarapidana = 84;
-var lakiLakiNarapidana = 82;
-var perempuanNarapidana = 2;
+// Memanggil fungsi aturDataMarquee() saat dokumen selesai dimuat
+window.onload = function () {
+    // Menunda penampilan teks di dalam marquee dengan delay 2000 milidetik (2 detik)
+    setTimeout(aturDataMarquee, 2000);
+};
 
-// Membuat teks dengan data yang diatur
-var newText = `Jumlah Total Warga Binaan Pemasyarakatan ${totalWBP} Orang dengan rincian ${jumlahTahanan} Orang Tahanan (${lakiLakiTahanan} Laki-Laki, ${perempuanTahanan} Perempuan) dan ${jumlahNarapidana} Orang Narapidana (${lakiLakiNarapidana} Laki-Laki, ${perempuanNarapidana} Perempuan).`;
+function aturDataMarquee() {
+    // Ambil elemen marquee
+    var marquee = document.getElementById('marqueeData');
 
-// Menampilkan teks dalam elemen dengan id "marqueeText"
-document.getElementById("marqueeText").textContent = newText;
+    // Data yang ingin diatur
+    var totalWBP = 146;
+    var jumlahTahanan = 62;
+    var lakiLakiTahanan = 60;
+    var perempuanTahanan = 2;
+    var jumlahNarapidana = 84;
+    var lakiLakiNarapidana = 82;
+    var perempuanNarapidana = 2;
+
+    // Format teks baru untuk dimasukkan ke dalam marquee
+    var newText = `Jumlah Total Warga Binaan Pemasyarakatan ${totalWBP} Orang dengan rincian ${jumlahTahanan} Orang Tahanan (${lakiLakiTahanan} Laki-Laki, ${perempuanTahanan} Perempuan) dan ${jumlahNarapidana} Orang Narapidana (${lakiLakiNarapidana} Laki-Laki, ${perempuanNarapidana} Perempuan).`;
+
+    // Mengganti teks di dalam marquee dengan teks baru yang sudah diformat
+    marquee.innerText = newText;
+};
